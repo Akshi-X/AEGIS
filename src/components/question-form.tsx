199aa869770d821151829cb6fbba7c6914459541
sidharthm <sidharthbavode@gmail.com>
2025-09-22 19:56:35 +0000

    TypeError: (0 , __TURBOPACK__imported__module__$5b$project$5d2f$node_mod

--- src/components/question-form.tsx
+++ src/components/question-form.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useActionState, useFormStatus } from 'react';
+import { useActionState, useFormStatus } from 'react-dom';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
